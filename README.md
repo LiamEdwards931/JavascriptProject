3240a799a7053c51e614c97b0deb3329134b928c
LiamEdwards931 <>
2023-07-02 09:48:55 +0000

    add and draw the meat collectible object to the canvas

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ![CI logo](https://codeinstitute.s3.amazonaws.com/fullstack/ci_logo_small.png)
 
 ## Bugs
-- Player Rex character would float back to the left edge of screen after continuous key presses (fixed) - was a result of "ghost typing" with personal laptop.
+- Player Rex character would float back to the left edge of screen after continuous key presses (fixed) - was a result of a missing bracket on the event listener IF for the keydown.
+- Player character wasn't animating correctly (fixed) - was missing a statement to reset frame interval back to 0 after it had stopped counting.
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -16,13 +16,13 @@ window.addEventListener('load', function () {
         constructor() {
             this.keys = [];
             window.addEventListener('keydown', a => {
-                if (a.key === 'ArrowDown' ||
+                if ((a.key === 'ArrowDown' ||
                     a.key === 'ArrowUp' ||
                     a.key === 'ArrowLeft' ||
-                    a.key === 'ArrowRight'
+                    a.key === 'ArrowRight')
                     && this.keys.indexOf(a.key) === -1) {
                     this.keys.push(a.key);
-                    console.log(this.keys,a.key);
+                    console.log(this.keys, a.key);
                 }
             });
             window.addEventListener('keyup', a => {
@@ -60,18 +60,18 @@ window.addEventListener('load', function () {
         draw(context) {
             //context.fillStyle = 'white';
             //context.fillRect(this.x, this.y, this.width, this.height);
-            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y - 60, this.width, this.height);
         }
         update(input, deltaTime) {
             //animation for the rex character
-            if(this.frameTimer > this.frameInterval){
-            if (this.frameX >= this.maxFrame) this.frameX = 0;
-            else this.frameX++;
-            this.frameTimer = 0;
+            if (this.frameTimer > this.frameInterval) {
+                if (this.frameX >= this.maxFrame) this.frameX = 0;
+                else this.frameX++;
+                this.frameTimer = 0;
             } else {
-                this.frameTimer += deltaTime
+                this.frameTimer += deltaTime;
             };
-          // controls for the rex char 
+            // controls for the rex char 
             if (input.keys.indexOf('ArrowRight') > -1) {
                 this.speed = 5;
             } else if (input.keys.indexOf('ArrowLeft') > -1) {
@@ -143,10 +143,10 @@ window.addEventListener('load', function () {
         }
         draw(context) {
             //(image, sx, sy, sw, sh, dx, dy, dw, dh) - crops and places the spritesheet
-            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height);
+            context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y - 60, this.width, this.height);
         }
         update(deltaTime) {
-            if(this.frameTimer > this.frameInterval){
+            if (this.frameTimer > this.frameInterval) {
                 if (this.frameX >= this.maxFrame) this.frameX = 0; //value increased by 1 up to max of 7, this is then multiplied in thre sourceX to continuously swap the position of the spritesheet which animates it.
                 else this.frameX++;
                 this.frameTimer = 0;
@@ -154,8 +154,8 @@ window.addEventListener('load', function () {
                 this.frameTimer += deltaTime;
             }
             this.x -= this.speed; //Moves enemies in from the left at the interval of what this.speed is.
-            if(this.x < 0 - this.width) this.markedForRemove = true; //removes egg enemy from array once they hit the 0 co-ordinate on X-axis.
-               
+            if (this.x < 0 - this.width) this.markedForRemove = true; //removes egg enemy from array once they hit the 0 co-ordinate on X-axis.
+
         }
     };
     //Meat collectible object
@@ -163,12 +163,15 @@ window.addEventListener('load', function () {
         constructor(gameWidth, gameHeight) {
             this.gameWidth = gameWidth;
             this.gameHeight = gameHeight;
-            this.x = 0;
-            this.y = 0;
             this.width = 180;
             this.height = 180;
+            this.x = 0;
+            this.y = 0;
             this.image = document.getElementById('meat');
-            
+            this.speed = 5;
+        }
+        draw(context) {
+            context.drawImage(this.image, this.x, this.y);
         }
     }
     /**Pushed new enemies into the array every time eggTimer hits the eggInteveral variable and resets it back to 0 to count again when it does.
@@ -192,7 +195,7 @@ window.addEventListener('load', function () {
     const rexChar = new Rex(canvas.width, canvas.height);
     const background = new Background(canvas.width, canvas.height);
     const eggEnemy = new EggEnemy(canvas.width, canvas.height);
-    const meat = new Meat();
+    const meat = new Meat(canvas.width, canvas.height);
 
     //time for when last egg spawn was done.
     let lastTime = 0;
@@ -213,6 +216,7 @@ window.addEventListener('load', function () {
         rexChar.draw(ctx);
         rexChar.update(input, deltaTime);
         Spawns(deltaTime);
+        meat.draw(ctx);
         requestAnimationFrame(animate);
     };
     animate(0); // needs a starting value for the animate "timestamp" or it's 1st value will be undefined.
